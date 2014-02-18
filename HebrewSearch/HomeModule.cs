b2793f827882b6c3aa2f844686ff7b5700c1a7d4
synhershko <itamar@code972.com>
2014-02-18 02:12:20 +0200

    Search now works

--- HebrewSearch/HomeModule.cs
+++ HebrewSearch/HomeModule.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using HebrewSearch.Models;
 using Nancy;
+using Nancy.ViewEngines.Razor;
 using Nest;
 
 namespace HebrewSearch
@@ -12,20 +12,61 @@ namespace HebrewSearch
     {
         public HomeModule(IElasticClient elasticClient)
         {
-            Get["/", true] = async (p, ct) =>
+            Get["/"] = (p) =>
+                       {
+                           var vm = new HomeViewModel();
+
+                           return View["Search", vm];
+                       };
+            
+            Get["/search/{q*}", true] = async (p, ct) =>
                        {
                            var vm = new HomeViewModel();
 
                            string q = p.q;
 
                            if (q != null) { 
-                               var results = elasticClient.SearchAsync<ContentPage>(
-                                   search => search.AllIndices().Query(
-                                        query => query.Bool(b => b.Must(bc => bc.Match(_ => _.Analyzer("hebrew_query_light").OnField("title").QueryString(q))))
-                                       )
+                               var results = await elasticClient.SearchAsync<ContentPage>(
+                                   search => search.Type("contentpage").AllIndices().Query(
+                                       mainQuery => mainQuery.Filtered(filtered => filtered.Query(
+                                        query => query.Bool(b => b.Should(
+                                            bc => bc.Match(_ => _.OnField("title").Boost(5.0).QueryString(q)),
+                                            bc => bc.Match(_ => _.OnField("text").QueryString(q))
+                                            )
+                                          )
+                                         ).Filter(
+                                            filter => filter.And(_ => _.Missing("redirect"))
+                                         )
+                                       ))
+                                       .Highlight(h => h.PreTags("<b>").PostTags("</b>").OnFields(_ => _.OnField("title").NumberOfFragments(0), _ => _.OnField("text")))
+                                       .Fields("title", "categories", "author")
                                    );
-                               //await Task.WhenAll(results);
-                               vm.Results = results.Result;
+
+                               if (results.ConnectionStatus.Error != null)
+                               {
+                                   vm.ErrorString = results.ConnectionStatus.Error.ToString();
+                               }
+
+                               vm.TotalResults = results.Total;
+                               vm.Results = new List<SearchResult>();
+                               foreach (var hit in results.DocumentsWithMetaData)
+                               {
+                                   var r = new SearchResult();
+                                   if (hit.Highlights.ContainsKey("title"))
+                                   {
+                                       r.Title = new NonEncodedHtmlString(String.Join("... ", hit.Highlights["title"].Highlights));
+                                   }
+                                   else
+                                   {
+                                       r.Title = new NonEncodedHtmlString(hit.Fields.Title);
+                                   }
+
+                                   if (hit.Highlights.ContainsKey("text"))
+                                   {
+                                       r.Snippets = new NonEncodedHtmlString(String.Join("... ", hit.Highlights["text"].Highlights));
+                                   }
+                                   vm.Results.Add(r);
+                               }
                            }
 
 
@@ -36,7 +77,9 @@ namespace HebrewSearch
 
     public class HomeViewModel
     {
+        public int TotalResults { get; set; }
         public string[] Indexes { get; set; }
-        public IEnumerable<ContentPage> Results { get; set; }
+        public List<SearchResult> Results { get; set; }
+        public string ErrorString { get; set; }
     }
 }
---
+++ HebrewSearch/Models/SearchResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy.ViewEngines.Razor;
+
+namespace HebrewSearch.Models
+{
+    public class SearchResult
+    {
+        public string Id { get; set; }
+        public IHtmlString Title { get; set; }
+        public string[] Categories { get; set; }
+        public IHtmlString Snippets { get; set; }
+    }
+}
